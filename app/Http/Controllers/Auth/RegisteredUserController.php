b00210f26768797100e5bb13020285d1bb2dec28
Joyan Barai <baraijoyan@gmail.com>
2021-08-23 10:49:31 +0600

    migration file's name is changed by Joyan Barai

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -43,7 +43,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'account_role' => $request->account_role,
+            // 'account_role' => $request->account_role,
         ]);
 
         event(new Registered($user));
